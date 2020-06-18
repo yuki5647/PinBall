3e198544039f761cc3b3b2f6c6f4ef65c8be1e3b
yukitosi <t.yuki.0527@gmail.com>
2020-06-18 15:36:21 +0900

    PinBallプロジェクト　初めてコミット
    初めて自作のピンボールゲーム

---
+++ Assets/BrightnessRegulator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrightnessRegulator : MonoBehaviour {
+
+    Material myMaterial;
+
+    private float minEmission = 0.3f;
+
+    private float magEmission = 2.0f;
+
+    private int degree = 0;
+
+    private int speed = 10;
+
+    Color defaultColor = Color.white;
+
+    // Use this for initialization
+    void Start() {
+
+        if (tag == "SmallstarTag")
+        {
+            this.defaultColor = Color.white;
+        }
+        else if (tag == "LargestarTag")
+        {
+            this.defaultColor = Color.yellow;
+        }
+        else if (tag == "SmallCloudTag" || tag == "LergeCloudTag")
+        {
+            this.defaultColor = Color.cyan;
+        }
+        this.myMaterial = GetComponent<Renderer>().material;
+
+        myMaterial.SetColor("_EmissionColor", this.defaultColor * minEmission);
+    }
+	// Update is called once per frame
+	void Update () {
+
+        if (this.degree >= 0)
+        {
+           
+            Color emissionColor = this.defaultColor * (this.minEmission + Mathf.Sin(this.degree * Mathf.Deg2Rad) * this.magEmission);
+
+            myMaterial.SetColor("_EmissionColor", emissionColor);
+
+            this.degree -= this.speed;
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        this.degree = 180;
+    }
+}
---
+++ Assets/ScoreController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour {
+
+    private int Score = 0;
+
+   void OnCollisionEnter (Collision other)
+    {
+        if (other.gameObject.tag == "SmallstarTag")
+        {
+            
+            Score += 20;
+        }
+        else if (other.gameObject.tag == "LargestarTag")
+        {
+            Score += 80;
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            Score += 10;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            Score += 120;
+        }
+
+
+        this.ScoreText.text = Score.ToString();
+    }
+    private Text ScoreText;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+       
+
+        this.ScoreText = GameObject.Find("ScoreText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+
+}
